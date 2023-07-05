d96bef2e57a97313813edd7e13508fab59055e48
garenboz <38819076+garenboz@users.noreply.github.com>
2023-07-05 16:03:05 +0300

    Update Layout.tsx

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -36,14 +36,60 @@ const Layout = () => {
             <header className={styles.header} role={"banner"}>
                 <div className={styles.headerContainer}>
                     <Stack horizontal verticalAlign="center">
-                        
+                        <img
+                            src={Azure}
+                            className={styles.headerIcon}
+                            aria-hidden="true"
+                        />
                         <Link to="/" className={styles.headerTitleContainer}>
-                            <h1 className={styles.headerTitle}>Azure AI</h1>
+                            <h1 className={styles.headerTitle}>Botomotiv</h1>
                         </Link>
+                        <div className={styles.shareButtonContainer} role="button" tabIndex={0} aria-label="Share" onClick={handleShareClick} onKeyDown={e => e.key === "Enter" || e.key === " " ? handleShareClick() : null}>
+                            <ShareRegular className={styles.shareButton} />
+                            <span className={styles.shareButtonText}>Share</span>
+                        </div>
                     </Stack>
                 </div>
             </header>
-            <Outlet />            
+            <Outlet />
+            <Dialog 
+                onDismiss={handleSharePanelDismiss}
+                hidden={!isSharePanelOpen}
+                styles={{
+                    
+                    main: [{
+                        selectors: {
+                          ['@media (min-width: 480px)']: {
+                            maxWidth: '600px',
+                            background: "#FFFFFF",
+                            boxShadow: "0px 14px 28.8px rgba(0, 0, 0, 0.24), 0px 0px 8px rgba(0, 0, 0, 0.2)",
+                            borderRadius: "8px",
+                            maxHeight: '200px',
+                            minHeight: '100px',
+                          }
+                        }
+                      }]
+                }}
+                dialogContentProps={{
+                    title: "Share the web app",
+                    showCloseButton: true
+                }}
+            >
+                <Stack horizontal verticalAlign="center" style={{gap: "8px"}}>
+                    <TextField className={styles.urlTextBox} defaultValue={window.location.href} readOnly/>
+                    <div 
+                        className={styles.copyButtonContainer} 
+                        role="button" 
+                        tabIndex={0} 
+                        aria-label="Copy" 
+                        onClick={handleCopyClick}
+                        onKeyDown={e => e.key === "Enter" || e.key === " " ? handleCopyClick() : null}
+                    >
+                        <CopyRegular className={styles.copyButton} />
+                        <span className={styles.copyButtonText}>{copyText}</span>
+                    </div>
+                </Stack>
+            </Dialog>
         </div>
     );
 };
