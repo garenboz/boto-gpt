db91c5109698b28c4963b0d49b5793107684a61e
garenboz <38819076+garenboz@users.noreply.github.com>
2023-07-05 10:51:45 +0300

    Update Layout.tsx

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -1,1 +1,37 @@
+import { Outlet, Link } from "react-router-dom";
+import styles from "./Layout.module.css";
+import Azure from "../../assets/Azure.svg";
+import { Dialog, Stack, TextField } from "@fluentui/react";
+import { useEffect, useState } from "react";
 
+const Layout = () => {
+    const [isSharePanelOpen, setIsSharePanelOpen] = useState<boolean>(false);
+    const [copyClicked, setCopyClicked] = useState<boolean>(false);
+    const [copyText, setCopyText] = useState<string>("Copy URL");
+
+    const handleShareClick = () => {
+        setIsSharePanelOpen(true);
+    };
+
+    const handleSharePanelDismiss = () => {
+        setIsSharePanelOpen(false);
+        setCopyClicked(false);
+        setCopyText("Copy URL");
+    };
+
+    const handleCopyClick = () => {
+        navigator.clipboard.writeText(window.location.href);
+        setCopyClicked(true);
+    };
+
+    useEffect(() => {
+        if (copyClicked) {
+            setCopyText("Copied URL");
+        }
+    }, [copyClicked]);
+
+    return (
+    );
+};
+
+export default Layout;
